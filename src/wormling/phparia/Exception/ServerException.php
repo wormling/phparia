871eba70793af593eb7a3fba436b96d7916759f7
Brian Smith <brian.smith@oncallcentral.com>
2015-05-13 22:15:10 +0300

    Added ServerException to createChannel in the case that a channel could not be created.

---
+++ src/wormling/phparia/Exception/ServerException.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright 2014 Brian Smith <wormling@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace phparia\Exception;
+
+use Exception;
+
+/**
+ * @author Brian Smith <wormling@gmail.com>
+ * 
+ * The error code is actially 500-599
+ */
+class ServerException extends Exception
+{
+
+    public function __construct(\Exception $e)
+    {
+        parent::__construct(json_decode($e->getMessage())->message, 500, $e->getPrevious());
+    }
+
+}
