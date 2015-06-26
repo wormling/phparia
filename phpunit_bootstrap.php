892849e92062d13141d544aa2683a80401d4131f
Brian Smith <brian.smith@oncallcentral.com>
2015-06-26 11:44:35 +0300

    Added phpunit configuration

---
+++ phpunit_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+$loader = @include __DIR__ . '/vendor/autoload.php';
+if (!$loader) {
+    $loader = require __DIR__ . './vendor/autoload.php';
+}
+$loader->addPsr4('phparia\\Tests\\Functional\\', __DIR__);
