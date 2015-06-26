ee361b7f3f04fd05ded27f00519e77f09f3aa5fd
Brian Smith <brian.smith@oncallcentral.com>
2015-06-26 12:37:10 +0300

    Fields in AsteriskInfo may be null

--- src/wormling/phparia/Resources/AsteriskInfo.php
+++ src/wormling/phparia/Resources/AsteriskInfo.php
@@ -74,8 +74,10 @@ class AsteriskInfo extends Response
 
         $this->buildInfo = property_exists($this->response, 'build') ? new BuildInfo($this->response->build) : null;
         $this->configInfo = property_exists($this->response, 'config') ? new ConfigInfo($this->response->config) : null;
-        $this->statusInfo = property_exists($this->response, 'statusInfo') ? new StatusInfo($this->response->statusInfo) : null;
-        $this->systemInfo = property_exists($this->response, 'systemInfo') ? new SystemInfo($this->response->systemInfo) : null;
+        $this->statusInfo = property_exists($this->response,
+            'statusInfo') ? new StatusInfo($this->response->statusInfo) : null;
+        $this->systemInfo = property_exists($this->response,
+            'systemInfo') ? new SystemInfo($this->response->systemInfo) : null;
     }
 
 }
