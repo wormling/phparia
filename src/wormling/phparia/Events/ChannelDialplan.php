7e95ff8546a4586c6ee1f9867e88718022c65fc4
Brian Smith <brian.smith@oncallcentral.com>
2015-06-27 17:05:01 +0300

    Added missing channel dialplan event!

---
+++ src/wormling/phparia/Events/ChannelDialplan.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * Copyright 2014 Brian Smith <wormling@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace phparia\Events;
+
+use phparia\Client\AriClient;
+use phparia\Resources\Channel;
+
+/**
+ * Channel changed location in the dialplan.
+ *
+ * @author Brian Smith <wormling@gmail.com>
+ */
+class ChannelDialplan extends Event implements IdentifiableEventInterface
+{
+    /**
+     * @var Channel
+     */
+    private $channel;
+
+    /**
+     * @var string The application about to be executed.
+     */
+    private $dialplanApp;
+
+    /**
+     * @var string The data to be passed to the application.
+     */
+    private $dialplanAppData;
+
+    /**
+     * @return Channel
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    public function getEventId()
+    {
+        return "{$this->getType()}_{$this->getChannel()->getId()}";
+    }
+
+    /**
+     * @param AriClient $client
+     * @param string $response
+     */
+    public function __construct(AriClient $client, $response)
+    {
+        parent::__construct($client, $response);
+
+        $this->channel = new Channel($client, $this->response->channel);
+        $this->dialplanApp = $this->response->dialplan_app;
+        $this->dialplanAppData = $this->response->dialplan_app_data;
+    }
+}
