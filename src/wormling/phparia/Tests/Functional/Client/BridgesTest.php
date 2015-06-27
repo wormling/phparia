0be84e6d7c5319c73b501eafd49beb9aa18a8ee3
Brian Smith <brian.smith@oncallcentral.com>
2015-06-27 17:56:21 +0300

    Added more functional tests

--- src/wormling/phparia/Tests/Functional/Client/BridgesTest.php
+++ src/wormling/phparia/Tests/Functional/Client/BridgesTest.php
@@ -2,8 +2,16 @@
 
 namespace {
 
+    use phparia\Events\StasisStart;
+    use phparia\Exception\NotFoundException;
+    use phparia\Resources\Bridge;
     use phparia\Tests\Functional\PhpariaTestCase;
 
+    /**
+     * Class BridgesTest
+     *
+     * @todo Refactor channel/bridge creation
+     */
     class BridgesTest extends PhpariaTestCase
     {
         /**
@@ -11,21 +19,197 @@ namespace {
          * @dataProvider bridgesDataProvider
          * @param $bridgeId
          * @param $bridgeName
+         * @return Bridge
          */
         public function canCreateBridge($bridgeId, $bridgeName)
         {
             $bridge = $this->client->bridges()->createBridge($bridgeId, null, $bridgeName);
             $this->assertInstanceOf('phparia\Resources\Bridge', $bridge);
+
+            return $bridge;
         }
 
         public function bridgesDataProvider()
         {
             return [
-                ['id-1000', 'name-1000'],
-                ['id-1001', 'name-1001'],
-                ['id-1002', 'name-1002'],
-                ['id-1003', 'name-1003'],
+                ['BRIDGE_1_ID', 'BRIDGE_1_NAME'],
+                ['BRIDGE_2_ID', 'BRIDGE_2_NAME'],
+                ['BRIDGE_3_ID', 'BRIDGE_3_NAME'],
             ];
+        }
+
+        /**
+         * @test
+         */
+        public function canGetBridges()
+        {
+            $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+            $this->client->bridges()->createBridge('BRIDGE_2_ID', null, 'BRIDGE_2_NAME');
+            $this->client->bridges()->createBridge('BRIDGE_3_ID', null, 'BRIDGE_3_NAME');
+            $bridges = $this->client->bridges()->bridges();
+            $this->assertGreaterThanOrEqual(3, count($bridges));
+            foreach ($bridges as $bridge) {
+                $this->assertInstanceOf('phparia\Resources\Bridge', $bridge);
+            }
+            $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+            $this->client->bridges()->deleteBridge('BRIDGE_2_ID');
+            $this->client->bridges()->deleteBridge('BRIDGE_3_ID');
+        }
+
+        /**
+         * @todo Enable this once bridge type/name can actually be updated
+         */
+        public function canUpdateBridge()
+        {
+            $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+            $updatedBridge = $this->client->bridges()->updateBridge('BRIDGE_1_ID', 'holding', 'BRIDGE_1_NEW_NAME');
+            $bridge = $this->client->bridges()->getBridge('BRIDGE_1_ID');
+            $this->assertEquals($updatedBridge, $bridge);
+        }
+
+        /**
+         * @test
+         */
+        public function canGetBridge()
+        {
+            $createdBridge = $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+            $bridge = $this->client->bridges()->getBridge('BRIDGE_1_ID');
+            $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+            $this->assertEquals($createdBridge, $bridge);
+        }
+
+        /**
+         * @test
+         * @expectedException phparia\Exception\NotFoundException
+         */
+        public function canGetBridgeThrowNotFoundException()
+        {
+            $this->client->bridges()->getBridge('THIS_BRIDGE_ID_WILL_NOT_EXIST');
+        }
+
+        /**
+         * @test
+         * @expectedException phparia\Exception\NotFoundException
+         */
+        public function canDeleteBridge()
+        {
+            $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+            $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+            $this->client->bridges()->getBridge('BRIDGE_1_ID');
+        }
+
+        /**
+         * @test
+         * @expectedException phparia\Exception\NotFoundException
+         */
+        public function canDeleteBridgeThrowNotFoundException()
+        {
+            $this->client->bridges()->deleteBridge('THIS_BRIDGE_ID_WILL_NOT_EXIST');
+        }
+
+        /**
+         * @test
+         */
+        public function canAddChannel()
+        {
+            $success = false;
+            $this->client->onStasisStart(function (StasisStart $event) use (&$success) {
+                $event->getChannel()->answer();
+                $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+                $this->client->bridges()->addChannel('BRIDGE_1_ID', $event->getChannel()->getId());
+                $channelIds = $this->client->bridges()->getBridge('BRIDGE_1_ID')->getChannels();
+                $found = false;
+                foreach ($channelIds as $channel) {
+                    if ($channel === $event->getChannel()->getId()) {
+                        $found = true;
+                    }
+                }
+                $this->assertTrue($found);
+                $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+                $success = true;
+                $this->client->stop();
+            });
+            $this->client->getAriClient()->onConnect(function () {
+                $this->client->channels()->createChannel($this->dialString, null, null, null, null,
+                    $this->client->getStasisApplicationName());
+            });
+            $this->client->run();
+            $this->assertTrue($success);
+        }
+
+        /**
+         * @test
+         * @expectedException phparia\Exception\NotFoundException
+         */
+        public function canAddChannelThrowNotFoundExceptionFromMissingBridge()
+        {
+            $success = false;
+            $this->client->onStasisStart(function (StasisStart $event) use (&$success) {
+                $event->getChannel()->answer();
+                $this->client->bridges()->addChannel('THIS_BRIDGE_ID_WILL_NOT_EXIST', $event->getChannel()->getId());
+                $this->client->stop();
+            });
+            $this->client->getAriClient()->onConnect(function () {
+                $this->client->channels()->createChannel($this->dialString, null, null, null, null,
+                    $this->client->getStasisApplicationName());
+            });
+            $this->client->run();
+        }
+
+        /**
+         * @test
+         * @expectedException phparia\Exception\NotFoundException
+         */
+        public function canAddChannelThrowNotFoundExceptionFromMissingChannel()
+        {
+            $success = false;
+            $this->client->onStasisStart(function (StasisStart $event) use (&$success) {
+                $event->getChannel()->answer();
+                $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+                try {
+                    $this->client->bridges()->addChannel('BRIDGE_1_ID', 'THIS_CHANNEL_WILL_NOT_EXIST');
+                } catch(NotFoundException $e) {
+                    $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+                    throw $e;
+                }
+                $this->client->stop();
+            });
+            $this->client->getAriClient()->onConnect(function () {
+                $this->client->channels()->createChannel($this->dialString, null, null, null, null,
+                    $this->client->getStasisApplicationName());
+            });
+            $this->client->run();
+        }
+
+        /**
+         * @test
+         */
+        public function canRemoveChannel()
+        {
+            $success = false;
+            $this->client->onStasisStart(function (StasisStart $event) use (&$success) {
+                $event->getChannel()->answer();
+                $this->client->bridges()->createBridge('BRIDGE_1_ID', null, 'BRIDGE_1_NAME');
+                $this->client->bridges()->addChannel('BRIDGE_1_ID', $event->getChannel()->getId());
+                $this->client->bridges()->removeChannel('BRIDGE_1_ID', $event->getChannel()->getId());
+                $channelIds = $this->client->bridges()->getBridge('BRIDGE_1_ID')->getChannels();
+                $found = false;
+                foreach ($channelIds as $channel) {
+                    if ($channel === $event->getChannel()->getId()) {
+                        $found = true;
+                    }
+                }
+                $this->assertFalse($found);
+                $this->client->bridges()->deleteBridge('BRIDGE_1_ID');
+                $success = true;
+                $this->client->stop();
+            });
+            $this->client->getAriClient()->onConnect(function () {
+                $this->client->channels()->createChannel($this->dialString, null, null, null, null,
+                    $this->client->getStasisApplicationName());
+            });
+            $this->client->run();
+            $this->assertTrue($success);
         }
     }
 }
