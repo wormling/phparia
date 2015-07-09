c751a961ffb2a19f643b058b77104be399b90901
Brian Smith <brian.smith@oncallcentral.com>
2015-07-09 10:34:42 +0300

    Quick refactor

--- src/wormling/phparia/Api/MediaBase.php
+++ src/wormling/phparia/Api/MediaBase.php
@@ -40,7 +40,7 @@ abstract class MediaBase extends AriClientAware
     /**
      * @return string 'channels' or 'bridges'
      */
-    public abstract function getType();
+    abstract public function getType();
 
     /**
      * Play music on hold to a channel. Using media operations such as /play on a channel playing MOH in
