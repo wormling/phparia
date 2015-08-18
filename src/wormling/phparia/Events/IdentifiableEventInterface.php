2497bbf893e068d749264f90d15b432e4723f1eb
Brian Smith <brian.smith@oncallcentral.com>
2015-08-18 07:14:05 +0300

    Typo correction

--- src/wormling/phparia/Events/IdentifiableEventInterface.php
+++ src/wormling/phparia/Events/IdentifiableEventInterface.php
@@ -27,7 +27,7 @@ interface IdentifiableEventInterface
 
     /**
      * @return string The id to use when emitting the event (should be specific instead of the general 
-     * event type as to route it correctly
+     * event type as to route it correctly)
      */
     public function getEventId();
 }
