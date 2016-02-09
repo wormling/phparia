ccbd05401ed380214ff960275fd8da3c64fe4e5f
Brian Smith <brian.smith@oncallcentral.com>
2016-02-09 11:04:14 +0200

    AriClient onClose test added

--- src/wormling/phparia/Tests/Functional/Api/AriClientTest.php
+++ src/wormling/phparia/Tests/Functional/Api/AriClientTest.php
@@ -70,5 +70,26 @@ namespace {
             $this->client->run();
             $this->assertTrue($success);
         }
+
+        /**
+         * @test
+         */
+        public function canCallOnClose()
+        {
+            $success = false;
+            $this->client->onStasisStart(function (StasisStart $event) {
+                $event->getChannel()->answer();
+                $this->client->stop();
+            });
+            $this->client->getAriClient()->onClose(function () use (&$success) {
+                $success = true;
+            });
+            $this->client->getAriClient()->onConnect(function () {
+                $this->client->channels()->createChannel($this->dialString, null, null, null, null,
+                    $this->client->getStasisApplicationName());
+            });
+            $this->client->run();
+            $this->assertTrue($success);
+        }
     }
 }
